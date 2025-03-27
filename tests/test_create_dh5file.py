68727be683482d0d3693a923a80bd37509e59ea3
Joscha Schmiedt <joscha@schmiedt.dev>
2025-03-27 10:51:12 +0100

    Rename create tests

--- tests/test_create_dh5file.py
+++ tests/test_create_dh5file.py
@@ -1,6 +1,8 @@
 import pytest
 from dh5io.create import create_dh5_file
+from dh5io.errors import DH5Warning
 from dh5io.validation import validate_dh5_file
+import warnings
 
 
 def test_create_dh5_file(tmp_path):
@@ -8,7 +10,9 @@ def test_create_dh5_file(tmp_path):
 
     dh5file = create_dh5_file(filename)
 
-    validate_dh5_file(dh5file)
+    with warnings.catch_warnings():
+        warnings.simplefilter("error", category=DH5Warning)
+        validate_dh5_file(dh5file)
 
     with pytest.raises(FileExistsError):
         create_dh5_file(filename)
