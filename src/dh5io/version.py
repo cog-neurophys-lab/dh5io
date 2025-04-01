1471a292a72cc25fb4fd6e7e0be3439d4ee4f39f
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-04-01 11:15:12 +0200

    Use importlib to retreive dh5io version

--- src/dh5io/version.py
+++ src/dh5io/version.py
@@ -1,10 +1,5 @@
-import tomllib
-import os
+import importlib.metadata
 
 
 def get_version():
-    here = os.path.dirname(__file__)
-    pyproject_path = os.path.join(here, "..", "pyproject.toml")
-    with open(pyproject_path, "rb") as f:
-        pyproject_data = tomllib.load(f)
-    return pyproject_data["project"]["version"]
+    return importlib.metadata.version("dh-format")
