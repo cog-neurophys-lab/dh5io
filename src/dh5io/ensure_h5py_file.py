14bc96a313cef88f9135f262c3915c62b3e3a5e0
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-04-02 09:52:57 +0200

    Allow DH5File as input to ensure_h5py_file decorated functions

--- src/dh5io/ensure_h5py_file.py
+++ src/dh5io/ensure_h5py_file.py
@@ -1,5 +1,6 @@
 import h5py
 import pathlib
+from dh5io.dh5file import DH5File
 
 
 def ensure_h5py_file(func, mode="r"):
@@ -9,6 +10,8 @@ def ensure_h5py_file(func, mode="r"):
                 return func(f, *args, **kwargs)
         elif isinstance(file, h5py.File):
             return func(file, *args, **kwargs)
+        elif isinstance(file, DH5File):
+            return func(file.file, *args, **kwargs)
         else:
             raise TypeError("file must be a h5py.File or a str or pathlib.Path")
 
